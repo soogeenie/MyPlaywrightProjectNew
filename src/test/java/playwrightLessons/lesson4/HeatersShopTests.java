b8acb9b6ad5a151523ed1fa42bb6953ab3ebe2c4
Natalia <fedchenko_n@yahoo.com>
2025-06-27 11:07:35 +1000

    added test

--- src/test/java/playwrightLessons/lesson4/HeatersShopTests.java
+++ src/test/java/playwrightLessons/lesson4/HeatersShopTests.java
@@ -10,5 +10,13 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 public class HeatersShopTests extends BaseTest {
     @Test
     public void searchByProductTest() {
+        //Fill INPUT WITH TEXT "Regency Renmark Wood Heater"
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Search")).fill("Regency Renmark Wood Heater");
+        //CLICK SEARCH
+        page.locator(".searchsubmit");
+        page.pause();
+        //VERIFY LINK IS VISIBLE
+        assertThat(page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Regency Renmark Wood Heater")))
+                .isVisible();
     }
 }
