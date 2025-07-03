7cd71ecb571239639709a8f90a7628e5e0d0acd4
Natalia <fedchenko_n@yahoo.com>
2025-07-03 14:10:05 +1000

    final version

--- src/test/java/demostoreTestProject/tests/ProductPageTests.java
+++ src/test/java/demostoreTestProject/tests/ProductPageTests.java
@@ -6,7 +6,6 @@ public class ProductPageTests extends BaseTest {
     @Test
     public void verifyAddToCartBtnIsVisible() {
         home.clickProduct("Samsung galaxy s6");  // returns product page
-        page.pause();
         product.verifyAddToCartIsVisible();
     }
 }
